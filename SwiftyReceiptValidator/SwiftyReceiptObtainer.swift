e9f7135ec9ee14cc0a46efde6ddb29f70881e391
Dom <Dominik@Doms-MacBook.local>
2017-02-24 19:12:52 +0000

    v2.1.3

--- SwiftyReceiptValidator/SwiftyReceiptObtainer.swift
+++ SwiftyReceiptValidator/SwiftyReceiptObtainer.swift
@@ -1,7 +1,6 @@
-
-//  Created by Dominik on 10/07/2016.
-
 //    The MIT License (MIT)
+//
+//    Copyright (c) 2016-2017 Dominik Ringler
 //
 //    Permission is hereby granted, free of charge, to any person obtaining a copy
 //    of this software and associated documentation files (the "Software"), to deal
