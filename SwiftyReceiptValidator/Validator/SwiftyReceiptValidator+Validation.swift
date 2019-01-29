4563f43ba58b36c52d5ab62998bc0bd875476758
Dominik Ringler <>
2019-01-29 20:34:02 +0000

    Cleanup.

---
+++ SwiftyReceiptValidator/Validator/SwiftyReceiptValidator+Validation.swift
@@ -0,0 +1,57 @@
+//
+//  SwiftyReceiptValidator+Internal.swift
+//  SwiftyReceiptValidator
+//
+//  Created by Dominik Ringler on 29/01/2019.
+//  Copyright © 2019 Dominik. All rights reserved.
+//
+
+import Foundation
+
+extension SwiftyReceiptValidator {
+    
+    func validate(_ response: SwiftyReceiptResponse,
+                  validationMode: ValidationMode,
+                  handler: @escaping ResultHandler) {
+        // Check receipt status is valid
+        guard response.status == .valid else {
+            handler(.failure(.invalidStatusCode, code: response.status))
+            return
+        }
+        
+        // Check receipt contains correct bundle id
+        guard response.receipt.bundleId == Bundle.main.bundleIdentifier else {
+            handler(.failure(.bundleIdNotMatching, code: response.status))
+            return
+        }
+        
+        // Run the validation for the correct mode
+        switch validationMode {
+            
+        case .none:
+            break
+            
+        case .purchase(let productId):
+            // Check a valid receipt with matching product id was found
+            guard response.receipt.inApp.first(where: { $0.productId == productId }) != nil else {
+                handler(.failure(.productIdNotMatching, code: response.status))
+                return
+            }
+            
+        case .subscription:
+            var receipts = response.latestReceiptInfo ?? response.receipt.inApp
+            receipts.removeAll {
+                guard let expiresDate = $0.expiresDate else { return true }
+                return expiresDate < Date()
+            }
+            
+            guard response.status != .subscriptionExpired, !receipts.isEmpty else {
+                handler(.failure(.noValidSubscription, code: response.status))
+                return
+            }
+        }
+        
+        // Return success handler
+        handler(.success(response))
+    }
+}
