b9b74798255c1a7dece6855469118ca119e0aa13
Dominik Ringler <>
2019-10-27 19:58:26 +0000

    Update to v5.0.1

--- build/SwiftyReceiptValidator.build/Debug-iphoneos/SwiftyReceiptValidator.build/DerivedSources/SwiftyReceiptValidator_vers.c
+++
@@ -1,5 +0,0 @@
- extern const unsigned char SwiftyReceiptValidatorVersionString[];
- extern const double SwiftyReceiptValidatorVersionNumber;
-
- const unsigned char SwiftyReceiptValidatorVersionString[] __attribute__ ((used)) = "@(#)PROGRAM:SwiftyReceiptValidator  PROJECT:SwiftyReceiptValidator-688" "\n";
- const double SwiftyReceiptValidatorVersionNumber __attribute__ ((used)) = (double)688.;
