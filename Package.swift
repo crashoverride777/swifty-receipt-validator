6892dd7e54d8ae19304e4ccc61777de0258a9e57
Dominik Ringler <>
2020-01-19 22:48:44 +0000

    SwiftPackage update and folder tweaks

--- Package.swift
+++ Package.swift
@@ -4,34 +4,31 @@
 import PackageDescription
 
 let package = Package(
-    name: "swifty-receipt-validator",
-    platforms: [.macOS(.v10_12),
-                .iOS(.v11)],
-    products: [.library(name: "swifty-receipt-validator",
-                        targets: ["swifty-receipt-validator"])],
-    targets: [.target(name: "swifty-receipt-validator")],
+    name: "SwiftyReceiptValidator",
+    platforms: [
+        .iOS(.v11)
+    ],
+    products: [
+        // Products define the executables and libraries produced by a package, and make them visible to other packages.
+        .library(
+            name: "SwiftyReceiptValidator",
+            targets: ["SwiftyReceiptValidator"]
+        )
+    ],
+    targets: [
+        // Targets are the basic building blocks of a package. A target can define a module or a test suite.
+        // Targets can depend on other targets in this package, and on products in packages which this package depends on.
+        .target(
+            name: "SwiftyReceiptValidator",
+            dependencies: []
+        ),
+        .testTarget(
+            name: "SwiftyReceiptValidatorTests",
+            dependencies: ["SwiftyReceiptValidator"]
+        ),
+    ],
     swiftLanguageVersions: [.v5])
-
-//let package = Package(
-//    name: "swifty-receipt-validator",
-//    products: [
-//        // Products define the executables and libraries produced by a package, and make them visible to other packages.
-//        .library(
-//            name: "swifty-receipt-validator",
-//            targets: ["swifty-receipt-validator"]),
-//    ],
 //    dependencies: [
 //        // Dependencies declare other packages that this package depends on.
 //        // .package(url: /* package url */, from: "1.0.0"),
 //    ],
-//    targets: [
-//        // Targets are the basic building blocks of a package. A target can define a module or a test suite.
-//        // Targets can depend on other targets in this package, and on products in packages which this package depends on.
-//        .target(
-//            name: "swifty-receipt-validator",
-//            dependencies: []),
-//        .testTarget(
-//            name: "swifty-receipt-validatorTests",
-//            dependencies: ["swifty-receipt-validator"]),
-//    ]
-//)
